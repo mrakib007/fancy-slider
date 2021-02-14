defd1c3e80c8d7a6cac109dbc6531d873b4ddb18
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-14 21:34:44 +0600

    added spinner

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
+    toggleSpinner();
   })
 
 }
 
 const getImages = (query) => {
+  toggleSpinner();
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+   //  toggleSpinner()
     .then(response => response.json())
     .then(data => showImages(data.hits))
     .catch(err => console.log(err))
@@ -152,3 +155,9 @@ document.getElementById("search")
         document.getElementById('search-btn').click();
       }
 });
+
+const toggleSpinner = () =>{
+  const spinner = document.getElementById('loading-spinner');
+  spinner.classList.remove('d-none');
+  spinner.classList.add('d-none');
+  }
